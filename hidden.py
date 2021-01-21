b35c83da5495a83341416d7cb92c7a0ff61b34aa
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-01-21 09:42:03 +0100

    api keys insertion commit

---
+++ hidden.py
@@ -0,0 +1,11 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+#insert your twitter api keys below
+
+def oauth():
+    return {"consumer_key": "_________________________",
+            "consumer_secret": "______________________________",
+            "token_key": "_________________________________",
+            "token_secret": "________________________________"}
